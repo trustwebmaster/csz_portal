d3ad848ad478d815481bd7705e529de94597a76a
eliphas1204 <masukaeliphas34@gmail.com>
2025-09-25 10:15:05 +0200

    temp bugfixes and on Paynow integration

--- app/Order.php
+++ app/Order.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace App\Models;
+namespace App;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
-{ 
-    protected $guarded = []; 
-    
+{
+    protected $guarded = [];
+
 }
